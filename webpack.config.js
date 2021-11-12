757a34546672317498b12e5d91a18fcf2498a5eb
Camilo Beltran <cambelf.dev@gmail.com>
2021-11-12 11:53:12 -0500

    :construction: Configuracion Webpack
    He relizado la configuracion inicial de webpack para el proyecto

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "build.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts",
+            publicPath: "./src/assets/fonts",
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      fileName: "./index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images/"),
+          to: "assets/images",
+        },
+        {
+          from: path.resolve(__dirname, "src", "assets/icons/"),
+          to: "assets/icons",
+        },
+        {
+          from: path.resolve(__dirname, "src", "data/"),
+          to: "./",
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserWebpackPlugin()],
+  },
+};
