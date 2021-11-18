3366dc4eda099bbcd308079bab3fb5930056bd46
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-18 12:42:56 -0500

    inicializando proyecto con vite para react

---
+++ solucion_desarrollador_m3/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hola mundo</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
