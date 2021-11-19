8c4f1c75b2d241ca82cc8f2ab4a78c5333d2eb9f
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-19 14:17:58 -0500

    agregando funcionalidad para recuperar el id del producto deseado

--- solucion_desarrollador_m3/src/components/ListaTarjetas/index.jsx
+++ solucion_desarrollador_m3/src/components/ListaTarjetas/index.jsx
@@ -1,11 +1,14 @@
 import Tarjeta from '../Tarjeta'
 import './listaTarjetas.css'
 
-export default function ListaTarjetas ({ data }) {
+export default function ListaTarjetas ({ data, agregar }) {
+  const agregarCarro = (id) => {
+    agregar(id)
+  }
   return (
     <div className='tarjetas'>
       {data.map((ele, i) => {
-        return <Tarjeta key={ele.id} src={ele.source} title='hola' price={ele.price} {...ele} />
+        return <Tarjeta key={ele.id} src={ele.source} title='hola' price={ele.price} agregar={() => agregarCarro(ele.id)} {...ele} />
       })}
     </div>
   )
