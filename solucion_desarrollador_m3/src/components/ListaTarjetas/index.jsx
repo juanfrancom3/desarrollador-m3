3638f9be6ad856165d2fff27c4810d1f00e468d3
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-22 16:34:40 -0500

    usando el hook useFiltroValor para filtrar los datos por color

--- solucion_desarrollador_m3/src/components/ListaTarjetas/index.jsx
+++ solucion_desarrollador_m3/src/components/ListaTarjetas/index.jsx
@@ -1,15 +1,38 @@
+import useFiltroValor from '../../hooks/useFiltroValor'
 import Tarjeta from '../Tarjeta'
 import './listaTarjetas.css'
 
-export default function ListaTarjetas ({ data, agregar }) {
+export default function ListaTarjetas ({ data, agregar, filtroColor, filtroTalla }) {
+  console.log('filtro', filtroColor)
+  console.log('filtro talla', filtroTalla)
   const agregarCarro = (id) => {
     agregar(id)
   }
+  let dataFil
+  if (filtroColor.length === 0) {
+    dataFil = data
+  } else {
+    dataFil = useFiltroValor({ data, key: 'colors', array: filtroColor })
+  }
+  // const dataF = data.filter((ele) => ele.colors.includes(filtro))
+  // console.log('dataF', dataF)
+  // const dd = new Set(data)
+  // dd.add(...dataF)
+  // const dataT = [...data, ...dataF]
+  // console.log(dataT)
+  // console.log(dataF)
+  // console.log(dataFil)
   return (
     <div className='tarjetas'>
-      {data.map((ele, i) => {
-        return <Tarjeta key={ele.id} src={ele.source} title='hola' price={ele.price} agregar={() => agregarCarro(ele.id)} {...ele} />
-      })}
+      {dataFil.map((ele, i) => (
+        <Tarjeta
+          key={ele.id}
+          src={ele.source}
+          title='hola'
+          price={ele.price}
+          agregar={() => agregarCarro(ele.id)} {...ele}
+        />)
+      )}
     </div>
   )
 }
