3aa302a7b667eecea647818080c58df73bd0768a
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-23 08:39:04 -0500

    agregando los filtros para talla y color

--- solucion_desarrollador_m3/src/App.jsx
+++ solucion_desarrollador_m3/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 // import logo from './logo.svg'
 import './App.css'
 import BolsaCompra from './components/BolsaCompra'
+import FiltroValor from './components/FiltroValor'
 import ListaTarjetas from './components/ListaTarjetas'
 import Titulo from './components/Titulo'
 // import Tarjeta from './components/Tarjeta'
@@ -9,13 +10,40 @@ import Titulo from './components/Titulo'
 import * as response from './data/data.json'
 function App () {
   const [compra, setCompra] = useState('')
+  const [filtroColor, setFiltroColor] = useState([])
+  const [filtroTalla, setFiltroTalla] = useState([])
   // const [a, setA] = useState({})
 
   const agregar = (id) => {
-    console.log('todo', id)
-
+    // console.log('todo', id)
     // console.log(a)
     setCompra(e => +e + 1)
+  }
+  const handleChangeFiltroColor = (event) => {
+    const newValue = event.target.value
+    console.error(newValue)
+    if (filtroColor.includes(newValue)) {
+      const set = new Set(filtroColor)
+      set.delete(newValue)
+      const valueNew = [...set]
+      setFiltroColor([...valueNew])
+    } else {
+      setFiltroColor([...filtroColor, newValue])
+    }
+  }
+
+  const handleChangeFiltroTalla = (event) => {
+    const newValue = event.target.value
+    console.error(newValue)
+    if (filtroTalla.includes(newValue)) {
+      const set = new Set(filtroTalla)
+      set.delete(newValue)
+      const valueNew = [...set]
+      setFiltroTalla([...valueNew])
+    } else {
+      setFiltroTalla([...filtroTalla, newValue])
+    }
+    // console.log(filtroColor)
   }
   return (
     <div className='App'>
@@ -26,11 +54,32 @@ function App () {
         </div>
       </header>
       <hr />
-      <div className='container main'>
-        <Titulo>Blusas</Titulo>
-        <p>ordenar</p>
+      <div className='container '>
+        <div className='main'>
+          <div className='titulo'>
+            <Titulo>Blusas</Titulo>
+          </div>
+          <p className='ordenar'>ordenar</p>
+          <div className='filtros'>
+            <FiltroValor elements={['azul', 'verde', 'branco', 'amarelo', 'cinza']} title='CORES' filtro={handleChangeFiltroColor} />
+
+            <FiltroValor elements={['P', 'M', 'G', 'GG', 'U', '36', '38', '40', '42', '44', '46']} title='CORES' filtro={handleChangeFiltroTalla} />
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+            <p>h</p>
+          </div>
+          <div className='catalogo'>
+            <ListaTarjetas data={response.data} filtroColor={filtroColor} filtroTalla={filtroTalla} agregar={agregar} />
+          </div>
+
+        </div>
       </div>
-      <ListaTarjetas data={response.data} agregar={agregar} />
+
     </div>
   )
 }
