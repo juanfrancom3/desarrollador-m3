e5a06fd7d7ce0cf07b4916212577928df10c9fab
Diego Fernando Alayon <dfar.41@gmail.com>
2021-11-23 15:48:04 -0500

    actualizando la data

--- solucion_desarrollador_m3/src/hooks/useFiltroValor.jsx
+++ solucion_desarrollador_m3/src/hooks/useFiltroValor.jsx
@@ -4,7 +4,6 @@
 export default function useFiltroValor ({ data, key, array }) {
   const response = (element, array) => {
     const res = array.map((filtro) => element.includes(filtro))
-    // console.log(res)
     return res.includes(true)
   }
   const dataFiltrada = data.filter(element => response(element[key], array))
