77bf83ebaa77334a7e7e2bc4b5b9f2eb68f785fa
Camilo Beltran <cambelf.dev@gmail.com>
2021-11-23 17:51:54 -0500

    :construction: ADD: funcionalidad de filtro en desktop
    He agregado la funcionalidad de filtrar los productos por color, precio y talla en la vista de escritorio

--- src/index.js
+++ src/index.js
@@ -147,10 +147,9 @@ document.getElementById("moreColors").addEventListener("click", function () {
   }
 });
 
-//   =========================================   //
-//     Programacion del formulario de filtro     //
-//   =========================================
-//
+//   ===============================================   //
+//     Programacion del formulario de filtro MOBILE    //
+//   ===============================================   //
 document
   .getElementById("btn_apply-filter")
   .addEventListener("click", async () => {
@@ -163,21 +162,6 @@ document
     const selectedSizes = document.querySelectorAll(
       "#filter_sizes input[type='checkbox']:checked"
     );
-    console.log(
-      "ARREGLO DE inputs sizes ",
-      selectedSizes,
-      selectedSizes.length
-    );
-    console.log(
-      "ARREGLO DE inputs prices ",
-      selectedPrices,
-      selectedPrices.length
-    );
-    console.log(
-      "ARREGLO DE inputs colors ",
-      selectedColors,
-      selectedColors.length
-    );
 
     const filtered = await getFilterValues(
       selectedColors,
@@ -186,42 +170,242 @@ document
     );
 
     const menu = document.getElementById("filter-menu");
-    window.scrollTo(0, 0); //subir la vista de la pagina a la parte superior
-    fadeOut(menu); //Cerrar menu de filtro
+    window.scrollTo(0, 0); // Subir la vista de la pagina a la parte superior
+    fadeOut(menu); // OCULTAR Menu de filtro
 
-    if (filtered.length == 0) {
-      //CREAR ELEMENTO HTML DE LA ALERTA
-      const alertContainer = document.createElement("div"); //CONTENEDOR
-      alertContainer.classList.add("alert_container"); //AGREGAR CLASE AL ELEMENTO HTML
+    isEmpty(filtered);
 
-      const parallax = document.createElement("div");
-      parallax.classList.add("parallax"); //AGREGAR CLASE AL ELEMENTO HTML
+    // ================ Elimar alerta
+    const alertCloser = document.getElementById("close_alert");
+    alertCloser.addEventListener("click", () => {
+      document.body.removeChild(parallax);
+    });
+  });
+//   =====================================================   //
+//     FIN DE Programacion del formulario de filtro MOBILE   //
+//   =====================================================   //
 
-      alertContainer.innerHTML += `
-        <div class="alert">
-          <img src="./assets/icons/close.png" id="close_alert" class="alert-closer"/>
-          <img src="./assets/images/signo-de-exclamacion.png" class="alert-image"/>
-          <div class="alert_text">
-            <p>Não há produtos que correspondam à pesquisa</p>
-          </div>
-        </div>
-      `;
-      parallax.appendChild(alertContainer);
-      document.body.appendChild(parallax);
+//   ===============================================   //
+//    Programacion del formulario de filtro Desktop    //
+//   ===============================================   //
 
-      // console.log(parallax);
+//Funcion para capturar los checkbox y separarlos de acuerdo la tipo de dato (color, size, price)
+function filterInDesktop() {
+  const checkboxCollection = document.querySelectorAll(
+    ".filter-menu_desktop .filter-menu_desktop--section input[type='checkbox']"
+  );
+  // console.log("Coleccion de checkbox: ", checkboxCollection);
 
-      //Elimar alerta
-      const alertCloser = document.getElementById("close_alert");
+  let selectedColors_desktop = [];
+  let selectedPrices_desktop = [];
+  let selectedSizes_desktop = [];
 
-      alertCloser.addEventListener("click", () => {
-        document.body.removeChild(parallax);
-      });
-    } else {
-      render(createProduct(filtered));
+  checkboxCollection.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      if (checkbox.classList.contains("hidde")) {
+        checkbox.classList.remove("hidde");
+        checkbox.classList.add("visible");
+        // console.log("Checkbox: ", checkbox);
+
+        if (checkbox.classList.contains("color")) {
+          selectedColors_desktop.push(checkbox);
+        } else if (checkbox.classList.contains("size")) {
+          selectedSizes_desktop.push(checkbox);
+        } else if (checkbox.classList.contains("price")) {
+          selectedPrices_desktop.push(checkbox);
+        }
+
+        getMyProducts(
+          selectedColors_desktop,
+          selectedPrices_desktop,
+          selectedSizes_desktop
+        );
+      } else {
+        checkbox.classList.add("hidde");
+        checkbox.classList.remove("visible");
+
+        if (checkbox.classList.contains("color")) {
+          selectedColors_desktop = eliminar(selectedColors_desktop, checkbox);
+        } else if (checkbox.classList.contains("price")) {
+          selectedPrices_desktop = eliminar(selectedPrices_desktop, checkbox);
+        } else if (checkbox.classList.contains("size")) {
+          selectedSizes_desktop = eliminar(selectedSizes_desktop, checkbox);
+        }
+
+        getMyProducts(
+          selectedColors_desktop,
+          selectedPrices_desktop,
+          selectedSizes_desktop
+        );
+      }
+    });
+  });
+}
+
+//Funcion para eliminar un elemento especifico  dentro de un array
+function eliminar(arreglo, elemento) {
+  let nuevoArreglo = [];
+  for (let i = 0; i < arreglo.length; i++) {
+    const element = arreglo[i];
+    if (element !== elemento) {
+      nuevoArreglo.push(element);
+    }
+  }
+  return nuevoArreglo;
+}
+
+async function getMyProducts(colors, prices, sizes) {
+  let allProducts = await getData();
+  let filtered = [];
+  //Condicionales para conocer el tipo de busqueda que se debe realizar de acuerdo a las opciones
+  //posibles de seleccion del menu
+  if (colors.length == 0 && prices.length == 0 && sizes.length == 0) {
+    filtered = allProducts;
+  } else if (colors.length != 0 && prices.length == 0 && sizes.length == 0) {
+    allProducts.forEach((product) => {
+      filtered = filterByColor(product, colors, filtered);
+    });
+  } else if (colors.length == 0 && prices.length != 0 && sizes.length == 0) {
+    allProducts.forEach((product) => {
+      filtered = filterByPrice(product, prices, filtered);
+    });
+  } else if (colors.length == 0 && prices.length == 0 && sizes.length != 0) {
+    allProducts.forEach((product) => {
+      filtered = filterBySize(product, sizes, filtered);
+    });
+  } else if (colors.length != 0 && prices.length == 0 && sizes.length != 0) {
+    let f_color = [];
+    let f_size = [];
+    allProducts.forEach((product) => {
+      f_color = filterByColor(product, colors, f_color);
+      f_size = filterBySize(product, sizes, f_size);
+    });
+    filtered = findAmatch(f_color, f_size);
+  } else if (colors.length != 0 && prices.length != 0 && sizes.length == 0) {
+    let f_color = [];
+    let f_price = [];
+    allProducts.forEach((product) => {
+      f_color = filterByColor(product, colors, f_color);
+      f_price = filterByPrice(product, prices, f_price);
+    });
+    filtered = findAmatch(f_color, f_price);
+  } else if (colors.length == 0 && prices.length != 0 && sizes.length != 0) {
+    let f_prices = [];
+    let f_sizes = [];
+    allProducts.forEach((product) => {
+      f_prices = filterByPrice(product, prices, f_prices);
+      f_sizes = filterBySize(product, sizes, f_sizes);
+    });
+    filtered = findAmatch(f_prices, f_sizes);
+  } else if (colors.length != 0 && prices.length != 0 && sizes.length != 0) {
+    let f_colors = [];
+    let f_prices = [];
+    let f_sizes = [];
+    allProducts.forEach((product) => {
+      f_colors = filterByColor(product, colors, f_colors);
+      f_prices = filterByPrice(product, prices, f_prices);
+      f_sizes = filterBySize(product, sizes, f_sizes);
+    });
+
+    filtered = findAmatch2(f_colors, f_prices, f_sizes);
+  }
+
+  filtered = [...new Set(filtered)];
+
+  isEmpty(filtered);
+
+  //Cerrar alerta en escritorio
+  if (document.getElementById("close_alert")) {
+    const alertCloser = document.getElementById("close_alert");
+    const parallax = document.querySelector(".parallax");
+    alertCloser.addEventListener("click", () => {
+      document.body.removeChild(parallax);
+    });
+  }
+}
+
+function findAmatch(array1, array2) {
+  let results = [];
+  for (let i = 0; i < array1.length; i++) {
+    let elemento1 = array1[i];
+    for (let j = 0; j < array2.length; j++) {
+      let elemento2 = array2[j];
+      if (elemento2 == elemento1) {
+        results.push(elemento2);
+      }
+    }
+  }
+  return results;
+}
+
+function findAmatch2(array1, array2, array3) {
+  let results = [];
+  for (let i = 0; i < array1.length; i++) {
+    let elemento1 = array1[i];
+    for (let j = 0; j < array2.length; j++) {
+      let elemento2 = array2[j];
+      for (let l = 0; l < array3.length; l++) {
+        const elemento3 = array3[l];
+        if (elemento1 == elemento2 && elemento2 == elemento3) {
+          results.push(elemento3);
+        }
+      }
+    }
+  }
+  return results;
+}
+
+function filterByColor(product, selectedColors, filtered) {
+  selectedColors.forEach((color) => {
+    if (color.value == product.color) {
+      filtered.push(product);
     }
   });
+  return filtered;
+}
 
+function filterByPrice(product, selectedPrices, filtered) {
+  selectedPrices.forEach((price) => {
+    price = parseInt(price.value);
+    if (price == 50) {
+      if (parseInt(product.price) > 0 && parseInt(product.price) <= price) {
+        filtered.push(product);
+      }
+    } else if (price == 150) {
+      if (parseInt(product.price) > 50 && parseInt(product.price) <= price) {
+        filtered.push(product);
+      }
+    } else if (price == 300) {
+      if (parseInt(product.price) > 150 && parseInt(product.price) <= price) {
+        filtered.push(product);
+      }
+    } else if (price == 500) {
+      if (parseInt(product.price) > 300 && parseInt(product.price) <= price) {
+        filtered.push(product);
+      }
+    } else {
+      if (parseInt(product.price) >= price) {
+        filtered.push(product);
+      }
+    }
+  });
+  return filtered;
+}
+
+function filterBySize(product, selectedSizes, filtered) {
+  selectedSizes.forEach((size) => {
+    if (product.sizes.includes(size.value)) {
+      filtered.push(product);
+    }
+  });
+  return filtered;
+}
+//   ======================================================   //
+//    FIM DE Programacion del formulario de filtro Desktop    //
+//   ======================================================   //
+
+// ==========  Funcion para mostrar los botones del menu de filtro em Mobile al momento de que se haga click en
+// algun elemento details.
 function showButtons() {
   const buttonsContainer = document.getElementById("filter-buttons");
   const detailsList = document.querySelectorAll("details");
@@ -232,6 +416,34 @@ function showButtons() {
     });
   });
 }
-showButtons();
 
+// ============= Funcion para imprimir en pantalla la alerta de que no existen productos disponibles bajo
+// low parametros indicados en el menu de filtro.
+function isEmpty(filtered) {
+  if (filtered.length == 0) {
+    //CREAR ELEMENTO HTML DE LA ALERTA
+    const alertContainer = document.createElement("div"); //CONTENEDOR
+    alertContainer.classList.add("alert_container"); //AGREGAR CLASE AL ELEMENTO HTML
+
+    const parallax = document.createElement("div");
+    parallax.classList.add("parallax"); //AGREGAR CLASE AL ELEMENTO HTML
+
+    alertContainer.innerHTML += `
+      <div class="alert">
+        <img src="./assets/icons/close.png" id="close_alert" class="alert-closer"/>
+        <img src="./assets/images/signo-de-exclamacion.png" class="alert-image"/>
+        <div class="alert_text">
+          <p>Não há produtos que correspondam à pesquisa</p>
+        </div>
+      </div>
+    `;
+    parallax.appendChild(alertContainer);
+    document.body.appendChild(parallax);
+  } else {
+    render(createProduct(filtered));
+  }
+}
+
+showButtons();
 render(getInfo());
+filterInDesktop();
