0a0069490ec0ba82ff39c81eaae9423e26ae532c
Alejandro Ríos <juanalcarios@gmail.com>
2021-12-01 00:01:26 -0500

    Filtros JS

--- README.md
+++ README.md
@@ -1,60 +1,23 @@
-# Ven a ser un desarrollador M3
+# Prueba desarrollador M3
 
-Hola somos M3, una agencia especializada en el campo de e-commerce. Trabajamos con despliegue, migración, evolución y optimización de tiendas de comercio electrónico. Nuestra casa matriz está en Brasil, Rio de Janeiro y contamos con oficinas comerciales en São Paulo, Buenos Aires y Bogotá.
+Mi nombre es Juan Alejandro Calderon Rios
+juacalderonr@correo.udistrital.edu.co
 
+## Visualización
+Para vizualizar el proyecto podemos hacerlo de 2 maneras simples
 
-Estamos buscando Desarrolladores(as) `Front-end`, con conocimienos sólidos en HTML, CSS y JS para diseñar interfaces y brindar la mejor experiencia a los clientes, buscando siempre la optimización del código y la agilidad en el desarrollo.
+La primera es directamente desde VS-Code, en este instalamos la extencion Live Server y al tenerla instalada en el codigo HTML damos click derecho y oprimimos en Open with live server
 
-## Qué estamos buscando:
+La segunda para una visualizacion mas agil podemos ver el proyecto en el siguiente host m3-test-shop.netlify.app/
 
-### Requisitos obligatorios:
+## Filtros
 
-- Html5, css3;
-- Javascript, jquery;
-- Versionamiento Git;
-- Grunt/gulp;
-- Sass/less;
-- Sites responsivos;
-- Iniciativa, creatividad y colaboración;
-- Buenas práticas: reutilización de código, semántica, organización, performance.
-
-### Deseables:
-
-- Experiencia con algún CMS
-- Conocimiento/ experiencia en Vtex
-- React
-
-----
-
-## Cómo aplicar a la vacante
-
-- [Crea un fork de este proyecto;](https://github.com/agenciam3/Desenvolvedor-M3/fork)
-- En el fork, codifica el layout siguiendo las instrucciones que están abajo;
-- Actualiza el readme con las instrucciones necesarias para ejecutar tu código;
-- Adiciona también tu e-mail de contacto al readme del proyecto ( en caso que tu e-mail de github sea privado )
-- Haz un pull request (o envíalo a [juan@m3ecommerce.com](mailto:juan@m3ecommerce.com?subject=Vacante%20DEV%20-%20M3)
+Podemos filtrar por color y precio
 
 
-**obs.**: link del proyecto [github.com/juanfrancom3/desarrollador-m3](https://github.com/juanfrancom3/desarrollador-m3)
+## Carrito de compra
+Se agrego un carrito de compras al que se puede acceder dando click en la bolsa ubicada en la parte superior derecha.
 
-### Instrucciones para la prueba
+En este podemos agregar o quitar los productos
 
-El layout se encontrá en la carpeta "layout" en PSD y PNG.
 
-### Obligatorio
-
-- HTML5 y CSS3
-- Interacción con JSON para renderizar los productos (siéntete libre de crear el JSON en el formato que encuentres más adecuado).
-- Interacción con JSON para filtrar los productos.
-- Funcionalidad: Filtrar productos por color, tamaño y precio.
-- Funcionalidad: Adicionar producto al carrito.
-- Funcionalidad: Agregar más productos.
-- No utilizar Bootstrap, Foundation Css, Semantic ui o semejantes ;
-- Responsivo
-
-#### Bonus
-
-- Javascript sin plugins y modular
-- Grunt, Gulp o similar
-
-##### Dudas: [juan@m3ecommerce.com](mailto:juan@m3ecommerce.com?subject=Consulta%20Vacante%20DEV%20-%20M3)
--- app.js
+++ app.js
@@ -28,6 +28,10 @@ const fetchData = async () => {
     try {
         const res = await fetch('blusas.json')
         const data = await res.json()
+        var elemento = document.getElementById("cards");
+        while (elemento.firstChild) {
+        elemento.removeChild(elemento.firstChild);
+        }
        /*  console.log(data) */
         printCards(data)
     } catch (error) {
@@ -41,7 +45,7 @@ const printCards = data =>{
         templateCards.querySelector('img').setAttribute('src', producto.imagen)
         templateCards.querySelector('h3').textContent = producto.nombre
         templateCards.querySelector('h4').textContent = producto.precio
-        templateCards.querySelector('p').textContent = producto.cuotas
+        templateCards.querySelector('p').textContent = producto.cuotas + Math.ceil(producto.precio/producto.ncuotas) 
         templateCards.querySelector('.btn-buy').dataset.id = producto.id
         const clone = templateCards.cloneNode(true)
         fragment.appendChild(clone)
@@ -54,7 +58,9 @@ const addCarrito = e =>{
     /* console.log(e.target.classList.contains('btn-buy')) */
     if(e.target.classList.contains('btn-buy')){
         setCarrito(e.target.parentElement)
+        alert("Se agregó al carrito")
     }
+    
     e.stopPropagation()
 }
 
@@ -153,3 +159,245 @@ const btnAccion = e => {
     }
     e.stopPropagation()
 }
+
+const openPop = document.getElementById('o-popup'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup')
+    closePop = document.getElementById('c-popup')
+
+openPop.addEventListener('click', () =>{
+    overlay.classList.add('active')
+})
+
+closePop.addEventListener('click', () =>{
+    overlay.classList.remove('active')
+})
+
+const allColors = document.getElementById('col-all')
+const filColorAm = document.getElementById('amarillo')
+const filColorAz = document.getElementById('azul')
+const filColorBl = document.getElementById('blanco')
+const filColorGr = document.getElementById('gris')
+const filColorNa = document.getElementById('naranja')
+
+allColors.addEventListener('click', () =>{
+    fetchData()
+})
+
+
+filColorAm.addEventListener('change', async () =>{
+    if (filColorAm.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.color == "amarillo")
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filColorAz.addEventListener('change', async () =>{
+    if (filColorAz.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.color == "azul")
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filColorBl.addEventListener('change', async () =>{
+    if (filColorBl.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.color == "blanco")
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filColorGr.addEventListener('change', async () =>{
+    if (filColorGr.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.color == "gris")
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filColorNa.addEventListener('change', async () =>{
+    if (filColorNa.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.color == "naranja")
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+const filPre1 = document.getElementById('pre1')
+const filPre2 = document.getElementById('pre2')
+const filPre3 = document.getElementById('pre3')
+const filPre4 = document.getElementById('pre4')
+const filPre5 = document.getElementById('pre5')
+
+filPre1.addEventListener('change', async () =>{
+    if (filPre1.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.precio <= 36000 && e.precio > 0)
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filPre2.addEventListener('change', async () =>{
+    if (filPre2.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.precio <= 108000 && e.precio > 36000)
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filPre3.addEventListener('change', async () =>{
+    if (filPre3.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.precio <= 216000 && e.precio > 108000)
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+filPre4.addEventListener('change', async () =>{
+    if (filPre4.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.precio <= 360000 && e.precio > 216000)
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
+
+
+filPre5.addEventListener('change', async () =>{
+    if (filPre5.checked) {
+        try {
+            const res = await fetch('blusas.json')
+            const data = await res.json()
+            const filtroCol = data.filter(e => e.precio > 1000)
+            console.log(filtroCol)
+            var elemento = document.getElementById("cards");
+            while (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+            }
+           /*  console.log(data) */
+            printCards(filtroCol)
+        } catch (error) {
+            console.log(error)
+        }
+      } else {
+        fetchData()
+      }
+});
